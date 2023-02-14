472ba0f637d28315cd6528a7786221c30226f9c4
Sasha Bédard <85566308+sashabedard@users.noreply.github.com>
2023-02-14 10:50:59 -0500

    fixed it

--- Arduino/light_sensor_v3/light_sensor_v3.ino
+++ Arduino/light_sensor_v3/light_sensor_v3.ino
@@ -1,15 +1,14 @@
 void setup() {
-  // initialize serial communication at 9600 bits per second:
+
  Serial.begin(9600);
 } 
 
 int value1 = 0;
-int value2 = 0;
+int value2 = 1;
 int value3 = 0;
-int value4 = 0;
+int value4 = 1;
 int value5 = 0;
-int value6 = 0;
-
+int value6 = 1;
 
 void loop() {
 
@@ -17,41 +16,14 @@ void loop() {
       value1 = 1;
   }
 
- if (analogRead(A1) < 800){
-      value2 = 1;
-  }
-
- if (analogRead(A2) < 800){
-      value3 = 1;
-  }
-
-   if (analogRead(A3) < 800){
-      value4 = 1;
-  }
-
-   if (analogRead(A4) < 800){
-      value5 = 1;
-  }
-
-  if (analogRead(A5) < 800){
-      value6 = 1;
-  }
-
-
 else{
-   value1 = 0;
-   value2 = 0;
-   value3 = 0;
-   value4 = 0;
-   value5 = 0;
-   value6 = 0;
+   value1 = 0 ;
 }
 
 print();
 }
 
 void print(){
-// If the laser is blocked by the user.
 Serial.print(value1);
  Serial.print(" ");
 Serial.print(value2);
@@ -64,5 +36,4 @@ Serial.print(value5);
  Serial.print(" ");
 Serial.println(value6);
 
-delay(10);
 } 
